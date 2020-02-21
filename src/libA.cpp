c9b28d9008bb764538cf4f4512e28f4825e05915
czoido <mrgalleta@gmail.com>
2020-02-21 18:33:58 +0100

    wip

--- src/libA.cpp
+++ src/libA.cpp
@@ -2,6 +2,7 @@
 #include "libA/libA.h"
 
 #include <iostream>
+// add comments
 
 void hello_libA(int indent, const std::string& msg) {
     std::cout << std::string(indent, ' ') << "libA: " << msg << std::endl;
