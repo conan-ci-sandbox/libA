78b95e883854e1622365bb6b1c01abe5e89ebcd7
czoido <mrgalleta@gmail.com>
2020-02-25 11:00:02 +0100

    remove comments

--- src/libA.cpp
+++ src/libA.cpp
@@ -3,13 +3,6 @@
 
 #include <iostream>
 
-/////
-/////////
-
-
-
-///////
-
 void hello_libA(int indent, const std::string& msg) {
     std::cout << std::string(indent, ' ') << "libA: " << msg << std::endl;
 }
